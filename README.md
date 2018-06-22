6149e77fda985562202400236665f46d50c93e2b
James Clancey <james.clancey@gmail.com>
2018-06-22 14:35:50 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Appcenter Flutter Sample
+
+To replicate, add the appcenter-post-clone.sh scripts from the iOS and android/app folders to your app. Configure the build as normal iOS/Android apps, and app center will do the rest!
---
+++ android/app/appcenter-post-clone.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
+
+cd ..
+cd ..
+
+git clone -b dev https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter doctor
+
+echo "Installed flutter to `pwd`/flutter"
+flutter build apk --release
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
